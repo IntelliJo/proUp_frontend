0fa3a8be2efda509c97a4cceb5dcc3574ea98c1c
k915k1004 <k915k1004@naver.com>
2022-03-26 00:25:13 +0900

    메인 페이지 초기 완성

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -11,9 +11,13 @@ const CenterS = styled.div`
   padding: 180px 0;
 `;
 const InputStyle = styled.input`
+  border-radius: 15px;
   width: 370px;
   height: 28px;
   border: none;
+`;
+const MainStyle = styled.div`
+  margin: 0px 20%;
 `;
 const FilterStyle = styled.div`
   display: flex;
@@ -26,6 +30,38 @@ const ListStyle = styled.div`
   width: 50px;
 `;
 
+const ProjectsStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+const ProjectStyle = styled.div`
+  margin: 5px 0px;
+  background-color: lightgray;
+  height: 140px;
+  width: 100%;
+  border-radius: 15px;
+`;
+
+const InnerStyle = styled.div`
+  box-sizing: border-box;
+  padding: 22px;
+  display: flex;
+  height: 100%;
+`;
+const TitleStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 2;
+  justify-content: center;
+`;
+const SubtitleStyle = styled(TitleStyle)`
+  flex: 1;
+  border-left: 1px solid gray;
+  padding-left: 5px;
+  height: 100%;
+`;
+
 const MainPage = () => (
   <div>
     <SearchStyle className="search">
@@ -33,7 +69,7 @@ const MainPage = () => (
         <InputStyle type="text"></InputStyle>
       </CenterS>
     </SearchStyle>
-    <div className="main">
+    <MainStyle className="main">
       <FilterStyle className="filter">
         <ListStyle>프레임워크</ListStyle>
         <ListStyle>프레임워크</ListStyle>
@@ -46,7 +82,23 @@ const MainPage = () => (
         <ListStyle>프레임워크</ListStyle>
         <ListStyle>프레임워크</ListStyle>
       </FilterStyle>
-    </div>
+      <ProjectsStyle className="projects">
+        {/* 프로젝트 시작 */}
+        <ProjectStyle className="project">
+          <InnerStyle>
+            <TitleStyle className="project-Title">
+              <span>Project Name</span>
+              <span>Project Content</span>
+            </TitleStyle>
+            <SubtitleStyle className="project-subTitle">
+              <span>Project Info</span>
+              <span>Programming Language</span>
+              <span>Recuitment</span>
+            </SubtitleStyle>
+          </InnerStyle>
+        </ProjectStyle>
+      </ProjectsStyle>
+    </MainStyle>
   </div>
 );
 
