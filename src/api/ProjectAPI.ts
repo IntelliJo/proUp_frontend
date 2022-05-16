6ac9652f478129b276cb6997eb4c307b67727da2
k915k1004 <k915k1004@naver.com>
2022-05-17 02:01:53 +0900

    codesmell

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -6,27 +6,23 @@ import { ProjectTS } from "../interfaces";
 // 프로젝트 목록 조회
 export const getProjectList = () => {
   const url = `/api/v1/proup/project?page=0&size=5&sort=id`;
-  const data = getApi(url);
-  return data;
+  return getApi(url);
 };
 
 // 프로젝트 등록 api
 export const postCreateProject = (body: ProjectTS) => {
   const url = `/api/v1/proup/project`;
-  const data = postApi(url, body);
-  return data;
+  return postApi(url, body);
 };
 
 // 프로젝트 수정 api
 export const putModifyProject = (id: string) => {
   const url = `/api/v1/proup/project/${id}`;
-  const data = patchApi(url);
-  return data;
+  return patchApi(url);
 };
 
 // 프로젝트 삭제 api
 export const deleteProject = (id: string) => {
   const url = `/api/v1/proup/project/${id}`;
-  const data = deleteApi(url);
-  return data;
+  return deleteApi(url);
 };
